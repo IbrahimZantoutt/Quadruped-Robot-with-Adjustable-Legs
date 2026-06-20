8ab5fe175a4fcde6564c6bb8b0e00cb72b620228
Ibrahim Zantout <kingofdeath3636@gmail.com>
2026-06-20 14:25:14 +0300

    first commit

---
+++ build/quadbot_nodes/CMakeFiles/JointMover.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JointMover.dir/src/JointMover.cpp.o"
+  "CMakeFiles/JointMover.dir/src/JointMover.cpp.o.d"
+  "JointMover"
+  "JointMover.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JointMover.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
